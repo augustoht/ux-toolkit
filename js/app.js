758c52e7ae3e737fd7bf8f3a5ff19523653ef672
Augusto Teixeira <augustoh.teixeira@gmail.com>
2013-12-09 15:42:42 -0200

    Build site at 2013-12-09 17:42:42 UTC

--- js/app.js
+++ js/app.js
@@ -1,10 +1,3 @@
-requirejs.config({    
-    baseUrl: 'js',    
-});
 
-require(['jquery/jquery'], function(){
-  require( ['bootstrap'], function(){
-    require( ['scripts'], function (){});
-  });  
-});
+
 
